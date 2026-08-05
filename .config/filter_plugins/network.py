7c5d1a3564da8f0fdc264f3cf4b5f32a1a0a73a6
vic1707 <28602203+vic1707@users.noreply.github.com>
2026-08-05 11:41:21 +0200

    cleanups

--- .config/filter_plugins/network.py
+++ .config/filter_plugins/network.py
@@ -1,10 +1,15 @@
 from ipaddress import ip_network
+from itertools import combinations
 
 
-def networks_overlap(left: str, right: str) -> bool:
-	return ip_network(left).overlaps(ip_network(right))
+def overlapping_networks(networks: list[str]) -> list[tuple[str, str]]:
+	return [(left, right) for left, right in combinations(networks, 2) if ip_network(left).overlaps(ip_network(right))]
+
+
+def icx_vlans(networks: list[dict]) -> list[dict]:
+	return [{"id": network["vlan_id"], "name": network["name"]} for network in networks]
 
 
 class FilterModule:
 	def filters(self) -> dict[str, object]:
-		return {"networks_overlap": networks_overlap}
+		return {"icx_vlans": icx_vlans, "overlapping_networks": overlapping_networks}
